34c619abf34438b860b618b26c19d5120a0b28d3
Tamer Aydin <tamer.aydin@gmail.com>
2016-02-15 23:08:25 +0100

    implemented check

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class PasswordRuler {
   }
 
   addLevel(level) {
-    this.levels.push({ validators: {} });
+    this.levels.push({ validator: {} });
 
     if (!level) {
       return this;
@@ -63,13 +63,63 @@ class PasswordRuler {
     levelIndex = typeof levelIndex === 'number' && levels[levelIndex] ?
       levelIndex : Math.max(0, levels.length - 1);
 
-    levels[levelIndex] = levels[levelIndex] || { validators: {} };
-    levels[levelIndex].validators[name] = {
+    levels[levelIndex] = levels[levelIndex] || { validator: {} };
+    levels[levelIndex].validator[name] = {
       validate: validate,
       weight: weight || 1
     };
 
     return this;
+  }
+
+  check(password) {
+    let totalValidatorCount = 0;
+    let totalValidValidatorCount = 0;
+    let isPreviousLevelValid = true;
+
+    let result = {
+      score: 0,
+      strength: 0
+    };
+
+    result.levels = this.levels.map((level) => {
+      let levelValidatorCount = 0;
+      let levelValidValidatorCount = 0;
+      let levelResult = {
+        score: 0,
+        validator: {}
+      };
+
+      Object.keys(level.validator).forEach((validatorName) => {
+        let validatorObj = level.validator[validatorName];
+        let isValid = levelResult.validator[validatorName] =
+          !!(isPreviousLevelValid &&
+            typeof password === 'string' &&
+            validatorObj.validate(password));
+
+        levelValidatorCount =+ validatorObj.weight;
+        totalValidatorCount =+ validatorObj.weight;
+
+        if (isValid) {
+          levelValidValidatorCount =+ validatorObj.weight;
+          totalValidValidatorCount =+ validatorObj.weight;
+        }
+      });
+
+      levelResult.score = levelValidatorCount ?
+        100 * levelValidValidatorCount / levelValidatorCount : 0;
+
+      if (levelResult.score === 100) {
+        isPreviousLevelValid = true;
+        result.strength++;
+      }
+
+      return levelResult;
+    });
+
+    result.score = totalValidatorCount ?
+      100 * totalValidValidatorCount / totalValidatorCount : 0;
+    return result;
   }
 }
 
--- test.js
+++ test.js
@@ -22,8 +22,7 @@ const fixtureLevelsWithMixedValidators = [{
     'weight': 1
   },
   'b': {
-    'validate': function() {},
-    'weight': 1
+    'validate': function() {}
   },
   'c': 1
 }, {
@@ -38,55 +37,69 @@ const fixtureImproperValidatorArgs4 = ['x', function() {}, 'y'];
 const fixtureProperValidatorArgs1 = ['a', function() {}];
 const fixtureProperValidatorArgs2 = ['b', function() {}, 2];
 const fixtureProperValidatorArgs3 = ['c', function() {}, 1, 99];
-// const fixtureInvalidPassword = 1;
-// const fixtureValidPassword = 'x';
+const fixtureInvalidPassword = 1;
+const fixturePassword1 = 'abc';
+const fixtureLevel1 = {
+  alwaysValid: {
+    validate: function() {
+      return true;
+    }
+  }
+};
 
 test('PasswordRuler()', t => {
-  let ruler1 = new PasswordRuler();
+  const ruler1 = new PasswordRuler();
 
   t.is(ruler1.levels.length, 0);
 
-  let ruler2 = new PasswordRuler(fixtureLevelWithProperValidator1);
+  const ruler2 = new PasswordRuler(fixtureLevelWithProperValidator1);
   t.is(ruler2.levels.length, 1);
-  t.ok(ruler2.levels[0].validators.x.validate);
-  t.is(ruler2.levels[0].validators.x.weight, 1);
+  t.ok(ruler2.levels[0].validator.x.validate);
+  t.is(ruler2.levels[0].validator.x.weight, 1);
 
-  let ruler3 = new PasswordRuler([
+  const ruler3 = new PasswordRuler([
     fixtureLevelWithProperValidator1,
     fixtureLevelWithProperValidator2
   ]);
   t.is(ruler3.levels.length, 2);
 
-  let ruler4 = new PasswordRuler([
+  const ruler4 = new PasswordRuler([
     fixtureLevelWithProperValidator1,
     fixtureLevelWithImproperValidator
   ]);
   t.is(ruler4.levels.length, 2);
 
-  let ruler5 = new PasswordRuler(fixtureLevelsWithMixedValidators);
+  const ruler5 = new PasswordRuler(fixtureLevelsWithMixedValidators);
   t.is(ruler5.levels.length, 3);
-  t.ok(ruler5.levels[0].validators.a);
-  t.ok(ruler5.levels[0].validators.b);
-  t.notOk(ruler5.levels[0].validators.c);
-  t.notOk(ruler5.levels[0].validators.c);
-  t.notOk(ruler5.levels[0].validators.d);
+  t.ok(ruler5.levels[0].validator.a);
+  t.ok(ruler5.levels[0].validator.b);
+  t.notOk(ruler5.levels[0].validator.c);
+  t.notOk(ruler5.levels[0].validator.c);
+  t.notOk(ruler5.levels[0].validator.d);
 });
 
 test('addLevel()', t => {
-  let aRuler = new PasswordRuler();
+  const aRuler = new PasswordRuler();
 
   aRuler.addLevel();
   t.same(aRuler.levels, [{
-    validators: {}
+    validator: {}
+  }]);
+
+  aRuler.addLevel(1);
+  t.same(aRuler.levels, [{
+    validator: {}
+  }, {
+    validator: {}
   }]);
 
   aRuler.addLevel(fixtureLevelWithProperValidator1);
-  t.is(aRuler.levels.length, 2);
-  t.ok(aRuler.levels[1].validators.x);
+  t.is(aRuler.levels.length, 3);
+  t.ok(aRuler.levels[2].validator.x);
 });
 
 test('addValidator()', t => {
-  let aRuler = new PasswordRuler();
+  const aRuler = new PasswordRuler();
 
   t.is(aRuler.addValidator.apply(aRuler, fixtureImproperValidatorArgs1)
     .levels.length, 0);
@@ -99,12 +112,43 @@ test('addValidator()', t => {
 
   aRuler.addValidator.apply(aRuler, fixtureProperValidatorArgs1);
   t.is(aRuler.levels.length, 1);
-  t.ok(aRuler.levels[0].validators.a);
+  t.ok(aRuler.levels[0].validator.a);
 
   aRuler.addValidator.apply(aRuler, fixtureProperValidatorArgs2);
-  t.ok(aRuler.levels[0].validators.b);
+  t.ok(aRuler.levels[0].validator.b);
 
   aRuler.addValidator.apply(aRuler, fixtureProperValidatorArgs3);
   t.is(aRuler.levels.length, 1);
-  t.ok(aRuler.levels[0].validators.c);
+  t.ok(aRuler.levels[0].validator.c);
+});
+
+test('check()', t => {
+  const emptyRuler = new PasswordRuler();
+  t.same(emptyRuler.check(1), {
+    'score': 0,
+    'strength': 0,
+    'levels': []
+  });
+
+  const ruler1 = new PasswordRuler(fixtureLevel1);
+  t.same(ruler1.check(fixtureInvalidPassword), {
+    'score': 0,
+    'strength': 0,
+    'levels': [{
+      'score': 0,
+      'validator': {
+        'alwaysValid': false
+      }
+    }]
+  });
+  t.same(ruler1.check(fixturePassword1), {
+    'score': 100,
+    'strength': 1,
+    'levels': [{
+      'score': 100,
+      'validator': {
+        'alwaysValid': true
+      }
+    }]
+  });
 });
